50ab4efcac44d0836e83c20991287982d6cb1fb6
Dave Arnold <darnold@hashicorp.com>
2018-09-24 15:58:14 -0700

    fixing keys

--- azure_k8s.tf
+++ azure_k8s.tf
@@ -28,9 +28,9 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   linux_profile {
     admin_username = "${azurerm_container_registry.cr.admin_username}"
-    ssh_key = [
-      "${file("${path.module}/files/azure_k8s.pub")}"
-    ]
+    ssh_key = {
+      key_data = "${file("${path.module}/files/azure_k8s.pub")}"
+    }
   }
 
   agent_pool_profile {
